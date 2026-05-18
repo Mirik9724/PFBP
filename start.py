9082ab1bf9b8ed9fe111d94b39dd10a8133599ce
Mirik9724 <mirik123465@gmail.com>
2026-05-18 18:12:47 +1000

    -

--- start.py
+++ start.py
@@ -3,7 +3,6 @@ sys.path.append(os.path.dirname(__file__))
 
 # from audi.au2Txt import *
 # from audi.aiTxt import *
-# from listenCmd import listC
 from cam import startCam
 from PFBP import *
 from webSer import app
@@ -12,8 +11,6 @@ async def start():
     # await asyncio.to_thread(loadModel)
     cam_thread = threading.Thread(target=startCam, daemon=True)
     cam_thread.start()
-    #
-    # listC()
 
     # await mdlIni()
     # lMt = threading.Thread(target=loadModel, daemon=True)
@@ -22,18 +19,7 @@ async def start():
 
     # await strLst()
 
-    move(True, True)
-    speed(50)
-    await asyncio.sleep(1)
-
-    move(False, False)
-    speed(50)
-    await asyncio.sleep(1)
-    stop()
-
-    # print("[INFO] Запуск веб-сервера на http://raspberrypi.local:5000")
-    # await asyncio.to_thread(uvicorn.run, app, host="0.0.0.0", port=5000, log_level="info")
-
-
+    print("[INFO] Запуск веб-сервера на http://raspberrypi.local:5000")
+    await asyncio.to_thread(uvicorn.run, app, host="0.0.0.0", port=5000, log_level="info")
 
 asyncio.run(start())
